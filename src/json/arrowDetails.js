6c84738e95dff4bf5c8be1befae42d3595d3e5c5
Carlos Diaz <cdiaz86@gmail.com>
2021-05-13 15:15:23 -0700

    Moves arrow details to json external file.

---
+++ src/json/arrowDetails.js
@@ -0,0 +1,13 @@
+import LeftArrow from '../images/left.png';
+import RightArrow from '../images/right.png';
+
+export const arrowDetails = [
+  {
+    direction: 'left',
+    image: LeftArrow
+  },
+  {
+    direction: 'right',
+    image: RightArrow
+  }
+];
