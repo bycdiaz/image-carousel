0437fe43fc2e2e352cc01f355e5f3807ab69af86
Carlos Diaz <cdiaz86@gmail.com>
2021-05-12 20:49:20 -0700

    Creates landing component.

---
+++ src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CarouselControls from '../components/CarouselControls';
+
+function Landing() {
+  return (
+    <CarouselControls />
+  );
+}
+
+export default Landing;
