4a35666f3c79b9c9617e7c1f1d2bbd9e64c02b10
Carlos Diaz <cdiaz86@gmail.com>
2021-05-13 16:45:07 -0700

    Removes header/footer info from layout function and imports it.

--- src/layouts/Standard.js
+++ src/layouts/Standard.js
@@ -1,36 +1,24 @@
-import React from "react";
-import Styled from "styled-components";
+import React from 'react';
+import Styled from 'styled-components';
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { headerFooterInfo } from '../json/headerFooterInfo';
 
 const PageStyle = Styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-
-  /* .content {
-    flex: 1 0 auto;
-  } */
-
-  /* .footer {
-    flex-shrink: 0;
-  } */
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
 `
 
 function StandardLayout(props) {
-  const titleInfo = {
-    title: "Cats at Window",
-    subtitle: "a short story"
-  }
-
   return (
     <PageStyle>
-      {/* <div className='content'> */}
-        <Header titleInfo={titleInfo}/>
-        {props.children}
-        <Footer className='footer' copyrightStatement="Copyright © 2021 Cats"/>
-      {/* </div> */}
+      <Header titleInfo={headerFooterInfo.header}/>
+      {props.children}
+      <Footer copyright={headerFooterInfo.footer.copyright}/>
     </PageStyle>
   )
 }
