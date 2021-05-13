f76ae99b0936335db7d86864e968f47af41b408e
Carlos Diaz <cdiaz86@gmail.com>
2021-05-13 16:43:59 -0700

    Removes unused styling.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,20 +8,15 @@ const FooterContainer = Styled.footer`
 
   font-weight: bold;
   color: #283040;
-
-  /* margin-top: 1rem; */
-  padding: 1rem;
   background-color: #BACDD9;
-  position: fixed;
-  bottom: 0;
-  /* left: 0; */
+  padding: 20px 0;
   width: 100vw;
 `
 
 function Footer(props) {
   return (
     <FooterContainer>
-      <h2>{props.copyrightStatement}</h2>
+      <h2>{props.copyright}</h2>
     </FooterContainer>
   )
 }
