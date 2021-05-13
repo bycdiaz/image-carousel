b61c83b89f127947e6b6c9f3e57e5155e241ca27
Carlos Diaz <cdiaz86@gmail.com>
2021-05-13 15:03:30 -0700

    Removes unneeded logic.

--- src/components/CarouselControls.js
+++ src/components/CarouselControls.js
@@ -1,42 +1,16 @@
 import React from 'react';
 import Styled from "styled-components";
 
-import Arrow from './Arrow';
-import LeftArrow from '../images/left.png';
-import RightArrow from '../images/right.png';
-
 const ArrowsContainer = Styled.div`
   display: flex;
   flex-flow: row no-wrap;
   justify-content: space-around;
 `
-const arrows = [
-  {
-    direction: 'left',
-    image: LeftArrow
-  },
-  {
-    direction: 'right',
-    image: RightArrow
-  }
-];
 
 function CarouselControls(props) {
-  function Arrows() {
-    return arrows.map(arrow => {
-      return (
-        <Arrow
-          arrowDetails={arrow}
-          key={arrow.direction}
-          captureArrowClick={props.captureArrowClick}
-        />
-      )
-    })
-  }
-
   return (
     <ArrowsContainer>
-      <Arrows />
+      {props.children}
     </ArrowsContainer>
   );
 }
