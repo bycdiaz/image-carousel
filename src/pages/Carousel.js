e739947b29a9bc9ffc62612c75fcaf9e41f2658c
Carlos Diaz <cdiaz86@gmail.com>
2021-05-13 15:16:40 -0700

    Removes arrow details. condenses captureArrowClick logic.

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -1,42 +1,27 @@
 import React, { useState } from 'react';
 
-import LeftArrow from '../images/left.png';
-import RightArrow from '../images/right.png';
+import { arrowDetails } from '../json/arrowDetails';
 import Arrow from '../components/Arrow';
 import CarouselControls from '../components/CarouselControls';
 
 function Carousel() {
   const [carouselIndex, setCarouselIndex] = useState(0);
   const [currentNumber, setCurrentNumber] = useState(0);
-
+  console.log(carouselIndex);
   const sampleArray = [0, 1, 2, 3, 4];
   function captureArrowClick(direction) {
-    let upcomingIndex = null;
     const index = direction === 'left' ? currentNumber - 1 : currentNumber + 1;
     setCurrentNumber(index);
 
     if (index % sampleArray.length >= 0) {
-      upcomingIndex = index % sampleArray.length
+      setCarouselIndex(index % sampleArray.length);
     } else {
-      upcomingIndex = (index % sampleArray.length) + sampleArray.length 
+      setCarouselIndex((index % sampleArray.length) + sampleArray.length); 
     }
-
-    setCarouselIndex(upcomingIndex);
   }
 
   function CarouselArrows() {
-    const arrows = [
-      {
-        direction: 'left',
-        image: LeftArrow
-      },
-      {
-        direction: 'right',
-        image: RightArrow
-      }
-    ];
-
-    return arrows.map(arrow => {
+    return arrowDetails.map(arrow => {
       return (
         <Arrow
           arrowDetails={arrow}
