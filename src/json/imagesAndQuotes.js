9abfcfa3081fa27b6026670abc41b5d03b51068d
Carlos Diaz <cdiaz86@gmail.com>
2021-05-12 18:33:08 -0700

    Adds images and cat quotes to json file.

---
+++ src/json/imagesAndQuotes.js
@@ -0,0 +1,33 @@
+import one from '../images/1.jpg';
+import two from '../images/2.jpg';
+import three from '../images/3.jpg';
+import four from '../images/4.jpg';
+import five from '../images/5.jpg';
+
+export const imagesAndQuotes = [
+  {
+    image: one,
+    catOneQuote: "See any birdies?",
+    catTwoQuote: "No",
+  },
+  {
+    image: two,
+    catOneQuote: "How about mices?",
+    catTwoQuote: "No",
+  },
+  {
+    image: three,
+    catOneQuote: "And now?",
+    catTwoQuote: "No",
+  },
+  {
+    image: four,
+    catOneQuote: "Is that them?",
+    catTwoQuote: "No!",
+  },
+  {
+    image: five,
+    catOneQuote: "How ab...",
+    catTwoQuote: "Gosh. Let me live!",
+  },
+];
