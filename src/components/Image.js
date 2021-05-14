874abafb5a768c6c29fcae192e83914f9dc33861
Carlos Diaz <cdiaz86@gmail.com>
2021-05-13 17:21:25 -0700

    Styles image.

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,6 +4,9 @@ import Styled from "styled-components";
 const ImageElement = Styled.img`
   min-width: 250px;
   max-width: 50%;
+
+  box-shadow: 10px 10px 25px #808080;
+  border-radius: 4%;
 `
 
 function Image(props) {
